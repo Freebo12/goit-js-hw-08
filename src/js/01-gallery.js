dfb683ec0c284b9c5098749ebd9c7fe35c8a1c63
Freebo12 <Tumechko12@gmail.com>
2022-12-28 13:41:28 +0100

    1-2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+const boxGallery = document.querySelector('.gallery');
+const markUpImage = createGalleryMarUp(galleryItems);
+
+boxGallery.insertAdjacentHTML('beforeend', markUpImage);
+
+function createGalleryMarUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>`;
+    })
+    .join('');
+}
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,28 @@
 import Player from '@vimeo/player';
 
 console.log(Player);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_LOCAL = 'videoplayer-current-time';
+
+player.on('timeupdate', function (data) {
+  const timeOff = JSON.stringify(data);
+  localStorage.setItem(KEY_LOCAL, timeOff);
+});
+
+const timeOn = JSON.parse(localStorage.getItem(KEY_LOCAL));
+const timeMove = timeOn.seconds;
+
+player
+  .setCurrentTime(timeMove)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });

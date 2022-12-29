b2acc96e3b12b99ba49b8fdbb16440f6bd576f0f
Freebo12 <Tumechko12@gmail.com>
2022-12-29 22:26:21 +0100

    fixed bugs (seconds)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,17 +4,15 @@ var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const KEY_LOCAL = 'videoplayer-current-time';
-// player.on('timeupdate', throttle(1000));
 player.on('timeupdate', function (data) {
   const timeOff = JSON.stringify(data);
   localStorage.setItem(KEY_LOCAL, timeOff);
 });
 
 const timeOn = JSON.parse(localStorage.getItem(KEY_LOCAL));
-const timeMove = timeOn.seconds;
 
 player
-  .setCurrentTime(timeMove)
+  .setCurrentTime(timeOn.seconds)
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {

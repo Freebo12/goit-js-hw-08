be65186623b5ec034a7f0300a6dd9460048cae00
Freebo12 <Tumechko12@gmail.com>
2023-01-02 19:11:49 +0100

    refactoring

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,58 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-form.addEventListener('input', throttle(onFormData, 500));
-form.addEventListener('submit', onSubmitForm);
+const STORAGE_KEY = 'feedback-form-state';
+let feedbackFormState = {};
 
-const formData = {};
+const formRef = document.querySelector('.feedback-form');
 
-function onFormData(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}
+formRef.addEventListener('input', throttle(storageUpdate, 500));
+formRef.addEventListener('submit', handleSubmitForm);
 
-function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-}
+storageStatusCheck();
 
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
+function storageStatusCheck() {
+  const currentValue = getFormCurrentValue();
+
+  if (currentValue?.email) {
+    feedbackFormState.email = formRef.email.value = currentValue.email;
   }
-})();
+
+  if (currentValue?.message) {
+    feedbackFormState.message = formRef.message.value = currentValue.message;
+  }
+}
+
+function storageUpdate(event) {
+  feedbackFormState[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
+}
+
+function handleSubmitForm(event) {
+  event.preventDefault();
+
+  if (!event.target.email.value || !event.target.message.value) {
+    return;
+  }
+
+  formDataOutput();
+  clearDataStorage();
+  event.currentTarget.reset();
+}
+
+function formDataOutput() {
+  if (getFormCurrentValue()) {
+    console.log(getFormCurrentValue());
+  }
+}
+
+function getFormCurrentValue() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY));
+}
+
+function clearDataStorage() {
+  if (getFormCurrentValue()) {
+    localStorage.removeItem(STORAGE_KEY);
+  }
+
+  feedbackFormState = {};
+}
